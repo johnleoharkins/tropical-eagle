e29705404c5beb22c6fc6c755c10be5025da501d
johnleoharkins <johnleoharkins003@gmail.com>
2023-03-11 14:03:00 -0600

    styling

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -7,22 +7,40 @@ import Page from "./Page";
 const Landing = () => {
     return (
         <Page>
-            <Grid2 container>
-                <Grid2 xs={7}>
-                    <div >
-                        <Typography>
-                            Hello,
-                        </Typography>
+            <Grid2 container direction={"column"}>
+                <Grid2 xs={12}>
+
+                    <div className={classes.name__container}>
                         <span className={classes.name_typography}>John Harkins</span>
                     </div>
+                </Grid2>
+                    <Grid2 container direction={"row"} columnSpacing={2} xs={12}>
+                        <Grid2 xs={8}>
+                            <Grid2>
+                                <Typography variant={'paragraph'}>
+                                    I am a web developer from Madison, Wisconsin. I’ve always followed my passions and let them guide me throughout life. A few things have remained constant: a love for technology, the brain, the environment, relationships and stories, history, comparative theology and philosophy, science and design. I’ve never been afraid of a challenge; I’ve learned from every outcome, whether glorious, tragic or otherwise, through rigorous, honest self-reflection and contemplation. I appreciate the good and the bad experiences because they have shaped me to be who I am today.
+                                </Typography>
+                            </Grid2>
+                            <Grid2>
+                                <Typography variant={'paragraph'}>
+                                    I am a very active individual and love to spend time outside. I love to be in nature, in my youth I found much enjoyment while camping, hunting, fishing and hiking. I enjoy living a healthy and active lifestyle – being active has always made me feel the best no matter what else life is throwing at me! I like to do anything that will get my blood pumping and get my adrenaline going such as snowboarding, riding motorcycles, getting a little risky and going fast.
+                                </Typography>
+                            </Grid2>
+                            <Grid2>
+                                <Typography variant={'paragraph'}>
+                                    If I had to give some major characteristics about myself it would be: deep thinker, problem solver, inclusive, curious, compassionate, outgoing, authentic and honest. I am dedicated and committed to whatever I apply myself to. I’m inspired and fascinated by ideas. It is exciting and sometimes daunting how there’s always more to learn that can help in one way or another in the future. I believe it is very valuable to know a little bit about a lot, this helps me to see new relationships and connections in all of what I do.
+                                </Typography>
+                            </Grid2>
+                        </Grid2>
+                        <Grid2 xs={4}>
+                            <TwitterTimeline screenName={"johnleoharkins"} listId={"1633478381579714561"} />
+                        </Grid2>
 
-                </Grid2>
-                <Grid2 xs={2}>
+                    </Grid2>
 
-                </Grid2>
-                <Grid2 xs={3}>
-                    <TwitterTimeline screenName={"johnleoharkins"} listId={"1633478381579714561"} />
-                </Grid2>
+
+
+
             </Grid2>
         </Page>
     )
